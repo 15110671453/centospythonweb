79de456175e99f5ff67040baaa09189ab718062c
15110671453 <15110671453@163.com>
2016-11-11 22:02:05 +0800

    commit python 11112016
    commit python 11112016

--- centospythondevelop/code.py
+++ centospythondevelop/code.py
@@ -1,9 +1,10 @@
+#!/usr/bin/python
 import web
 urls = ('/','index')
 
 class index:
 	def GET(self):
 		return "HELLO,world!python"
-if __name__ == "__main__": 
+if __name__ == "__code__": 
 		app = web.application(urls,globals())
 		app.run()
